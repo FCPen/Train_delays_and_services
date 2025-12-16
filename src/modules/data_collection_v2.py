9adf8f1e518809ad400da5926f4dc18ec21f154b
FCPen <fc.penfold@gmail.com>
2025-12-16 17:33:13 +0000

    Building out new data collection script and removing old ones

--- src/modules/data_collection_v2.py
+++ src/modules/data_collection_v2.py
@@ -1,3 +1,8 @@
+import requests
+import pandas as pd
+from browser_cookie3 import chrome
+from datetime import date
+
 COLUMNS = [
     "stp_indicator",
     "transport_type",
@@ -27,3 +32,60 @@ COLUMNS = [
     "num_vehicles",
 ]
 
+RUN_DATE = date(2024, 6, 1)   # change this
+CRS = "RDG"
+OUTFILE = f"RDG_{RUN_DATE}.csv"
+
+
+def mins(t):
+    if not t:
+        return None
+    return int(t[:2]) * 60 + int(t[2:])
+
+
+def delay(actual, planned):
+    if not actual or not planned:
+        return None
+    return mins(actual) - mins(planned)
+
+
+def extract_csv_row(svc, run_date):
+    ld = svc.get("locationDetail", {})
+
+    return {
+        "stp_indicator": svc.get("stpIndicator"),
+        "transport_type": "T",
+        "schedule_uid": svc.get("uid"),
+        "run_date": run_date.isoformat(),
+        "train_identity": svc.get("trainIdentity"),
+        "this_tiploc": ld.get("tiploc"),
+        "this_crs": CRS,
+        "origin_tiploc": svc["origin"][0]["tiploc"],
+        "origin_description": svc["origin"][0]["description"],
+        "destination_tiploc": svc["destination"][-1]["tiploc"],
+        "destination_description": svc["destination"][-1]["description"],
+        "gbtt_arr": ld.get("gbttBookedArrival"),
+        "gbtt_dep": ld.get("gbttBookedDeparture"),
+        "wtt_arr": ld.get("publicArrival"),
+        "wtt_dep": ld.get("publicDeparture"),
+        "wtt_pass": ld.get("publicPass"),
+        "actual_arr": ld.get("realtimeArrival"),
+        "actual_arr_delay_mins": delay(
+            ld.get("realtimeArrival"),
+            ld.get("gbttBookedArrival")
+        ),
+        "actual_dep": ld.get("realtimeDeparture"),
+        "actual_dep_delay_mins": delay(
+            ld.get("realtimeDeparture"),
+            ld.get("gbttBookedDeparture")
+        ),
+        "actual_pass": ld.get("realtimePass"),
+        "actual_pass_delay_mins": delay(
+            ld.get("realtimePass"),
+            ld.get("publicPass")
+        ),
+        "platform": ld.get("platform"),
+        "platform_actual": ld.get("platform"),
+        "lead_class": svc.get("leadClass"),
+        "num_vehicles": svc.get("vehicleCount"),
+    }
