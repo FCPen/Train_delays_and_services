fffe1d3d0e0350b0efa86368d7066d414aa43328
FCPen <fc.penfold@gmail.com>
2025-12-19 17:41:05 +0000

    Fixing the merging script

--- src/modules/merge_data.py
+++ src/modules/merge_data.py
@@ -4,9 +4,16 @@ from glob import glob
 files = glob(r"C:\Users\fcpen\Documents\Trains_project\Service_data_csv\location_gb-nr_RDNGSTN_*.csv") #returns a list of files following that pattern
 print(f"Found {len(files)} files")
 
-df = pd.concat(pd.read_csv(f) for f in files)
+dfs = []
+for f in files:
+    dfs.append(pd.read_csv(f, skiprows=2)) #ensures the real header is used
+
+df = pd.concat(dfs, ignore_index=True)
 
 df["run_date"] = pd.to_datetime(df["run_date"])
 df = df.sort_values(["run_date", "gbtt_dep", "gbtt_arr"])
+
+print(df.columns.tolist())
+print(len(df.columns.tolist()))
 
 df.to_csv(r"..\data\RDG_2024-2025_ALL.csv", index=False)
