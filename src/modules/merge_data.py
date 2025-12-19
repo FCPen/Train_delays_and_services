253760b66d21a3fa8836c88503dc0358ba18d943
FCPen <fc.penfold@gmail.com>
2025-12-19 17:28:50 +0000

    Creating a script to merge data

---
+++ src/modules/merge_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from glob import glob
+
+files = glob("C:\Users\fcpen\Documents\Trains_project\Service_data_csv\location_gb-nr_RDNGSTN_*.csv") #returns a list of files following that pattern
+print(f"Found {len(files)} files")
+
+df = pd.concat(pd.read_csv(f) for f in files)
+
+df["run_date"] = pd.to_datetime(df["run_date"])
+df = df.sort_values(["run_date", "gbtt_dep", "gbtt_arr"])
+
+df.to_csv("RDG_2024-2025_ALL.csv", index=False)
